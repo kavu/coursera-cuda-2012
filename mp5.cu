ba6cb6dc6876fe519aca805adb2aa65e69f49e74
Max Riveiro <kavu13@gmail.com>
2013-02-01 23:43:47 +0400

    Style fixes

--- mp5.cu
+++ mp5.cu
@@ -28,34 +28,34 @@ __global__ void post_scan(float* in, float* add, int len) {
 }
 
 __global__ void scan(float* in, float* out, float* post, int len) {
-    __shared__ float scan_array[HALF_BLOCK_SIZE];
-    unsigned int t = threadIdx.x;
-    unsigned int start = 2 * blockIdx.x * BLOCK_SIZE;
-    int index;
+  __shared__ float scan_array[HALF_BLOCK_SIZE];
+  unsigned int t = threadIdx.x;
+  unsigned int start = 2 * blockIdx.x * BLOCK_SIZE;
+  int index;
 
-    if (start + t < len) scan_array[t] = in[start + t];
-    else scan_array[t] = 0;
+  if (start + t < len) scan_array[t] = in[start + t];
+  else scan_array[t] = 0;
 
-    if (start + BLOCK_SIZE + t < len) scan_array[BLOCK_SIZE + t] = in[start + BLOCK_SIZE + t];
-    else scan_array[BLOCK_SIZE + t] = 0;
-    __syncthreads();
+  if (start + BLOCK_SIZE + t < len) scan_array[BLOCK_SIZE + t] = in[start + BLOCK_SIZE + t];
+  else scan_array[BLOCK_SIZE + t] = 0;
+  __syncthreads();
 
-    for (unsigned int stride = 1; stride <= BLOCK_SIZE; stride <<= 1) {
-       index = (t + 1) * stride * 2 - 1;
-       if (index < 2 * BLOCK_SIZE) scan_array[index] += scan_array[index - stride];
-       __syncthreads();
-    }
+  for (unsigned int stride = 1; stride <= BLOCK_SIZE; stride <<= 1) {
+     index = (t + 1) * stride * 2 - 1;
+     if (index < 2 * BLOCK_SIZE) scan_array[index] += scan_array[index - stride];
+     __syncthreads();
+  }
 
-    for (unsigned int stride = BLOCK_SIZE >> 1; stride; stride >>= 1) {
-       index = (t + 1) * stride * 2 - 1;
-       if (index + stride < 2 * BLOCK_SIZE) scan_array[index + stride] += scan_array[index];
-       __syncthreads();
-    }
+  for (unsigned int stride = BLOCK_SIZE >> 1; stride; stride >>= 1) {
+     index = (t + 1) * stride * 2 - 1;
+     if (index + stride < 2 * BLOCK_SIZE) scan_array[index + stride] += scan_array[index];
+     __syncthreads();
+  }
 
-    if (start + t < len) out[start + t] = scan_array[t];
-    if (start + BLOCK_SIZE + t < len) out[start + BLOCK_SIZE + t] = scan_array[BLOCK_SIZE + t];
+  if (start + t < len) out[start + t] = scan_array[t];
+  if (start + BLOCK_SIZE + t < len) out[start + BLOCK_SIZE + t] = scan_array[BLOCK_SIZE + t];
 
-    if (post && t == 0) post[blockIdx.x] = scan_array[2 * BLOCK_SIZE - 1];
+  if (post && t == 0) post[blockIdx.x] = scan_array[2 * BLOCK_SIZE - 1];
 }
 
 int main(int argc, char ** argv) {
