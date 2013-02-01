b53ac9acd2bc425e1ab6d58939181c5d8428b87d
Max Riveiro <kavu13@gmail.com>
2013-02-01 23:38:28 +0400

    Style fixes

--- mp2.cu
+++ mp2.cu
@@ -19,9 +19,9 @@
   (size-1)/THREADS + 1
 
 __global__ void matrixMultiply(float * A, float * B, float * C,
-             int numARows, int numAColumns,
-             int numBRows, int numBColumns,
-             int numCRows, int numCColumns) {
+                               int numARows, int numAColumns,
+                               int numBRows, int numBColumns,
+                               int numCRows, int numCColumns) {
   int row = blockIdx.y * blockDim.y + threadIdx.y;
   int col = blockIdx.x * blockDim.x + threadIdx.x;
   if ((row < numCRows) && (col < numCColumns)) {
